d26d62e834e750c95157cfb8550d89657d56a5fc
LokiSylvie <lokiofasgard685@gmail.com>
2024-09-09 13:54:28 +0530

    Hi Loki

---
+++ src/org/asgard/Odin.java
@@ -0,0 +1,13 @@
+package org.asgard;
+
+public class Odin {
+	private void tc1() {
+		System.out.println("TC1");
+	}
+	
+
+	public static void main(String[] args) {
+		Odin s=new Odin();
+		s.tc1();
+	}
+}
